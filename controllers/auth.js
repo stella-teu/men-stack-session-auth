46e03826dd86f2556fee0f6890dadec3272b8a93
StellaTeuscher <stella.teuscher@gmail.com>
2025-03-27 14:09:39 -0400

    finished lecture

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,14 @@ import { Router } from "express";
 const userRouter = Router();
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import isSignedIn from "../middleware/is-signed-in.js";
+
 
 //routes
+userRouter.get("/", (req,res) => {
+    res.render("index.ejs");
+});
+
 userRouter.get("/sign-up", (req, res) => {
     res.render("auth/sign-up.ejs");
 })
@@ -27,6 +33,13 @@ userRouter.post("/sign-up", async (req, res) => {
 
     const user = await User.create(req.body);
     res.send("Thanks for signing up " + user.username + ".");
+
+    req.session.user = {
+        username: user.username,
+    };
+    req.session.save(() => {
+        res.redirect("/");
+    })    
 })
 
 userRouter.get("/sign-in", (req, res) => {
@@ -43,6 +56,26 @@ userRouter.post("/sign-in", async (req, res) => {
     if (!validPassword) {
         return res.send("Login failed. Please try agin.");
     }
+    console.log("Valid password")
+
+    req.session.user = {
+        _id: userInDatabase._id,
+        username: userInDatabase.username
+    }
+
+    req.session.save(() => {
+        res.redirect("/");
+    })
+})
+
+userRouter.get("/sign-out", async (req, res) => {
+    req.session.destroy(() => {
+        res.redirect("/");
+    });
+})
+
+userRouter.get("/vip", isSignedIn, (req,res) => {
+    res.render("auth/vip-lounge.ejs")
 })
 
 export default userRouter;
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import "dotenv/config";
 import methodOverride from "method-override";
 import router from "./controllers/auth.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import passUserToView from "./middleware/pass-user-to-view.js";
 
 const app = express();
 const port = process.env.PORT ? process.env.PORT : "3000";
@@ -11,10 +14,19 @@ mongoose.connect(process.env.MONGODB_URI);
 app.use(express.urlencoded({ extended: false }));
 // Middleware for using HTTP verbs such as PUT or DELETE
 app.use(methodOverride("_method"));
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGODB_URI,
+        })
+    })
+);
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
+app.use(passUserToView);
+
 app.use("/", router);
 
 mongoose.connection.on("connected", ()=> {
